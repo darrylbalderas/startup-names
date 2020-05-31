2cd43c9e1b12f709c3e800b40bfd66e56385c225
darrylbalderas <drrylbalderas@gmail.com>
2020-05-30 23:06:00 -0700

    Generate translated word in top ten languages

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from googletrans import Translator
-translator = Translator()
-translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='ko')
+from googletrans.constants import LANGUAGES, LANGCODES
 
-for t in translations:
-    print(t.origin, t.text)
+
+def main():
+    translator = Translator()
+    top_languages = [(t.lower(), LANGCODES[t.lower()]) for t in [
+      "chinese (traditional)",
+      "chinese (simplified)",
+      "hindi",
+      "Spanish",
+      "Arabic",
+      "Malay",
+      "Russian",
+      "Bengali",
+      "Portuguese",
+      "French",
+      "Hausa",
+      "Punjabi",
+      "Telugu",
+      "Javanese"
+    ]]
+    print(f"Original word :{word}")
+    print("------------")
+    for index, l in enumerate(top_languages):
+        language, code = l
+        translate_word = translator.translate(word, dest=code)
+        print(f"{index + 1}. {language} translated word : {translate_word.text}")
+
+
+if __name__ == '__main__':
+    main()
